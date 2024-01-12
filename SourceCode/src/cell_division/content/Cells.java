345f4fcf1497147c59e38af6e583456db1b1aff2
nvdang1811 <nvandang1811@gmail.com>
2024-01-12 20:32:02 +0700

    Revamp some designs

--- SourceCode/src/cell_division/content/Cells.java
+++ SourceCode/src/cell_division/content/Cells.java
@@ -2,7 +2,7 @@ package cell_division.content;
 
 import java.util.ArrayList;
 
-public abstract class Cells implements Process{
+public abstract class Cells{
 	private String cellName;
 	private ArrayList<Components> components;
 	public abstract String getDirectory();
@@ -11,4 +11,7 @@ public abstract class Cells implements Process{
 	public abstract Double[] getComponentsX();
 	public abstract Double[] getComponentsY();
 	public abstract String getImage();
+	public abstract void Amitosis();
+	public abstract void Meiosis();
+	public abstract void Mitosis();
 }
